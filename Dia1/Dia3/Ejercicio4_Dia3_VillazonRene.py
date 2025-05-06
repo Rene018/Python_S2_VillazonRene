4bc41478548a92379b155166df497e5ffa4108c3
Rene018 <rene18agosto@gmail.com>
2025-05-06 12:45:07 -0500

    fix: :sparkles: fixing errors and changing file names

--- Dia1/Dia3/Ejercicio4_Dia3_VillazonRene.py
+++ Dia1/Dia3/Ejercicio4_Dia3_VillazonRene.py
@@ -1,13 +1,12 @@
 
 # Verificar si un número es primo
-numero = 17
-es_primo = True
+numero = 2
+esPrimo = True
 if numero < 2:
     esPrimo = False
 else:
     for i in range(2, int(numero**0.5) + 1):
-        print(int(numero**0.5)+1)
         if numero % i == 0:
-            es_primo = False
+            esPrimo = False
             break
 print('Es primo:', esPrimo)
